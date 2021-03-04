31abe91dc2f904fe9f1ad782ede690394e94dfa1
Joao Vicente <joao@upsites.digital>
2021-03-04 14:44:48 -0300

    Atualizando os arquivo para aprender a fazer PUT e a otimizar o código

--- resources/hotel.py
+++ resources/hotel.py
@@ -28,34 +28,42 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message': 'Hotel not found.'}, 404
 
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('diaria')
-        argumentos.add_argument('cidade')
 
-        dados = argumentos.parse_args()
+        dados = Hotel.argumentos.parse_args()
 
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'cidade': dados['cidade']
-        }
+        novo_hotel = {'hotel_id': hotel_id, **dados}
 
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
-    def put(self):
-        pass
+    def put(self, hotel_id):
 
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
     def delete(self):
         pass
