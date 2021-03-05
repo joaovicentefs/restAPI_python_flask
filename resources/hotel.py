213086152833e20a3dbc9311c27ec833f5a6bbf8
Joao Vicente <joao@upsites.digital>
2021-03-04 22:38:27 -0300

    Atualizando os arquivo para aprender a fazer Delete e a criar classes

--- resources/hotel.py
+++ resources/hotel.py
@@ -23,6 +23,23 @@ hoteis = [
         }
     ]
 
+class HotelModel:
+    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
+        self.hotel_id = hotel_id
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
+
+    def json(self):
+        return {
+            'hotel_id': self.hotel_id,
+            'nome': self.nome,
+            'estrelas': self.estrelas,
+            'diaria': self.diaria,
+            'cidade': self.cidade
+        }
+
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
@@ -47,23 +64,23 @@ class Hotel(Resource):
         return {'message': 'Hotel not found.'}, 404
 
     def post(self, hotel_id):
-
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     def put(self, hotel_id):
-
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
             return novo_hotel, 200
         hoteis.append(novo_hotel)
         return novo_hotel, 201
-    def delete(self):
-        pass
+    def delete(self, hotel_id):
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'hotel Deleted'}, 200
