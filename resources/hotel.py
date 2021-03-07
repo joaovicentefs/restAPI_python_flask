9195e58090409e25b8f59affbb50b3acc4107c2a
Joao Vicente <joao@upsites.digital>
2021-03-06 23:44:03 -0300

    Atualização já usando o Banco de dados criado usando o SQLAlchemy e foi atualizado o primeira requisição, POST para trabalhar direto no banco.

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,45 +1,16 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
-hoteis = [
-        {
-            'hotel_id': 'alpha',
-            'nome': 'Alpha Hotel',
-            'estrelas': 4.3,
-            'diaria': 420.34,
-            'cidade': 'Rio de Janeiro'
-        },
-        {
-            'hotel_id': 'bravo',
-            'nome': 'Bravo Hotel',
-            'estrelas': 4.4,
-            'diaria': 380.90,
-            'cidade': 'São Paulo'
-        },
-        {
-            'hotel_id': 'charlie',
-            'nome': 'Charlie Hotel',
-            'estrelas': 3.9,
-            'diaria': 320.20,
-            'cidade': 'São Paulo'
-        }
-    ]
 
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
 
 class Hotel(Resource):
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
-
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome')
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
 
     def get(self, hotel_id):
         hotel = Hotel.find_hotel(hotel_id)
@@ -48,14 +19,15 @@ class Hotel(Resource):
         return {'message': 'Hotel not found.'}, 404
 
     def post(self, hotel_id):
-        dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": f"Hotel id '{hotel_id}' already exists."}, 400
+        dados = Hotel.atributos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json(), 201
 
     def put(self, hotel_id):
-        dados = Hotel.argumentos.parse_args()
+        dados = Hotel.atributos.parse_args()
         hotel_objeto = HotelModel(hotel_id, **dados)
         novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
