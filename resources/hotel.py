f57af74f061de90d236ef35d5f47b7bfc6cb52fa
Joao Vicente <joao@upsites.digital>
2021-03-08 14:09:36 -0300

    Atualizando os métodos para que eles façam a operação no banco de dados e não na memória

--- resources/hotel.py
+++ resources/hotel.py
@@ -13,9 +13,9 @@ class Hotel(Resource):
     atributos.add_argument('cidade')
 
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found.'}, 404
 
     def post(self, hotel_id):
@@ -28,14 +28,15 @@ class Hotel(Resource):
 
     def put(self, hotel_id):
         dados = Hotel.atributos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json(), 201
+
     def delete(self, hotel_id):
         global hoteis
         hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
