ebdfc283563bc74118ad875d03682af21624a639
Joao Vicente <joao@upsites.digital>
2021-03-11 23:00:58 -0300

    Refatorando o código para implementar o delete usando os dados no banco de dados

--- resources/hotel.py
+++ resources/hotel.py
@@ -38,6 +38,8 @@ class Hotel(Resource):
         return hotel.json(), 201
 
     def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'hotel Deleted'}, 200
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            hotel.delete_hotel()
+            return {'message': 'Hotel deleted.'}, 200
+        return {'message': 'Hotel not found.'}, 404
