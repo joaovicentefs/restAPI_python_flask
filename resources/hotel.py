56990efcbf6417febe4f4c384a2c8e3eada24a22
Joao Vicente <joao@upsites.digital>
2021-03-18 23:51:46 -0300

    Aprendendo a usar JWT e fazendo o login do usuário atraves do token gerado

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
 
 
 class Hoteis(Resource):
@@ -20,9 +21,10 @@ class Hotel(Resource):
             return hotel.json()
         return {'message': 'Hotel not found.'}, 404
 
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
-            return {"message": f"Hotel id '{hotel_id}' already exists."}, 400
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
         dados = Hotel.atributos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
         try:
@@ -31,6 +33,7 @@ class Hotel(Resource):
             return {'message': 'An internal error ocurred trying to save hotel.'}, 500
         return hotel.json(), 201
 
+    @jwt_required()
     def put(self, hotel_id):
         dados = Hotel.atributos.parse_args()
         hotel_encontrado = HotelModel.find_hotel(hotel_id)
@@ -45,6 +48,7 @@ class Hotel(Resource):
             return {'message': 'An internal error ocurred trying to save hotel.'}, 500
         return hotel.json(), 201
 
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
